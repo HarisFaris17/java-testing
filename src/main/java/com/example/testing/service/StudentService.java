6ef5417a663518f4539391153d7da78e4dd8d11a
HarisFaris17 <harisfaris17@gmail.com>
2022-07-01 13:01:28 +0700

    menambahkan unit testing dari class StudentService. Unit testing ini membutuhkan mocking terhadap StudentRepository karena StudentService bergantung padanya

---
+++ src/main/java/com/example/testing/service/StudentService.java
@@ -0,0 +1,23 @@
+package com.example.testing.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.testing.model.Student;
+import com.example.testing.repository.StudentRepository;
+
+@Service
+public class StudentService {
+    private StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> getAllStudent(){
+        return studentRepository.findAll();
+    }
+}
